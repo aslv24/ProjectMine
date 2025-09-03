59abc411f9e178c281ec149c96af8c210e3d733d
saran25112 <murugesansaran43@gmail.com>
2025-09-03 17:23:32 +0530

    Project Modified
    1. driver folder created and drivers updated
    2. config properties file modified
    3. propertyreader class created
    4. baseclass updated

--- ProjectMine/src/main/java/com/pm/TestBase/BaseClass.java
+++ ProjectMine/src/main/java/com/pm/TestBase/BaseClass.java
@@ -1,5 +1,44 @@
 package com.pm.TestBase;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
 public class BaseClass {
 
+	private static ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
+
+	public static void setDriver(String browserName) {
+
+		if (browserName.equalsIgnoreCase("chrome")) {
+			System.setProperty(PropertyReader.getProperty("chromeKey"), PropertyReader.getProperty("chromeValue"));
+			driver.set(new ChromeDriver());
+		}
+
+		else if (browserName.equalsIgnoreCase("edge")) {
+			System.setProperty(PropertyReader.getProperty("edgeKey"), PropertyReader.getProperty("edgeValue"));
+			driver.set(new EdgeDriver());
+		}
+
+		else if (browserName.equalsIgnoreCase("firefox")) {
+			System.setProperty(PropertyReader.getProperty("firefoxKey"), PropertyReader.getProperty("firefoxValue"));
+			FirefoxOptions options = new FirefoxOptions();
+			options.addArguments("--allow-system-access");
+			driver.set(new FirefoxDriver(options));
+		}
+
+		else {
+			String errorMessage = "❌ Undefined Browser Setup: '" + browserName
+					+ "'. Please check browser configuration...";
+			System.err.println(errorMessage);
+			throw new IllegalArgumentException(errorMessage);
+		}
+	}
+
+	public static WebDriver getDriver() {
+		return driver.get();
+
+	}
 }
